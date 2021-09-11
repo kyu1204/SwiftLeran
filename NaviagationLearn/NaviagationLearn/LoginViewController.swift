5f4b175c75ef61fcaff8850c7bad48cdf8b9d077
mint <pb1123love@gmail.com>
2021-09-11 17:18:50 +0900

    navigation viewcontroller 연습 1

---
+++ NaviagationLearn/NaviagationLearn/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaviagationLearn
+//
+//  Created by 김민규 on 2021/08/30.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
