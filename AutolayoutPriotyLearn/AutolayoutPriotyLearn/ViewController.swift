ff4783b25cc8aa62021d770e457617cf5d66c8dd
mint <pb1123love@gmail.com>
2021-09-12 20:22:03 +0900

    오토레이아웃 우선순위, 허깅, 컴프레션 레지스턴스 연습

---
+++ AutolayoutPriotyLearn/AutolayoutPriotyLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutolayoutPriotyLearn
+//
+//  Created by 김민규 on 2021/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
